eea7755ae448c5892f72ae6b06a9874459bb2e7d
TeuerStein <xenkai123456789@gmail.com>
2020-11-02 13:57:02 +0100

    Initial commit. Added new properties in database. Was created a ToDoItem Model & added a getAllToDoItems static function in extension

---
+++ AppWithCoreData/ToDoItem.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoItem.swift
+//  AppWithCoreData
+//
+//  Created by ALEXANDR DRAGUNOV on 11/2/20.
+//
+
+import Foundation
+import CoreData
+
+public class ToDoItem: NSManagedObject, Identifiable {
+    @NSManaged public var created: Date?
+    @NSManaged public var title: String?
+}
+
+extension ToDoItem {
+    static func getAllToDoItems() -> NSFetchRequest<ToDoItem> {
+        let request: NSFetchRequest<ToDoItem> = ToDoItem.fetchRequest() as! NSFetchRequest<ToDoItem>
+        
+        let sortDescriptor = NSSortDescriptor(key: "createAt", ascending: true)
+        
+        request.sortDescriptors = [sortDescriptor]
+        
+        return request
+    }
+}
